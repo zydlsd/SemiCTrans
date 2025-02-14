0768a947ed58dd8989401eaf9399812eb1e7c6d2
ZYD <ydzhou@stu.xmu.edu.cn>
2025-02-14 14:50:28 +0800

    Add files via upload

---
+++ src/data/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py.py
+# @Time      :2025/2/12 10:43
+# @Author    :zyd
+
+
+if __name__ == "__main__":
+    run_code = 0
---
+++ src/utils/data_utils/loaders/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File    : __init__.py.py
+# @Time    : 9/26/23 11:09 PM
+# @Author  : zyd
+
+
+if __name__ == "__main__":
+    run_code = 0
